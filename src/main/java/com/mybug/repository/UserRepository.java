4a092bd38c3e52d38d5276ffa19197233bc475a8
Tienchyi <tq96321@163.com>
2017-12-22 17:46:25 +0800

    UserInfo对象

--- src/main/java/com/mybug/repository/UserRepository.java
+++ src/main/java/com/mybug/repository/UserRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * Created by Administrator on 2017/12/22.
+ * Created by TienChyi on 2017/12/22.
  */
 @Repository
 public interface UserRepository extends JpaRepository<UserInfoPo,Integer>{
+    UserInfoPo findById(Integer id);
 }
