c84ac425f63ce994ca50d4126e72965db935ae4f
Tienchyi <tq96321@163.com>
2017-12-22 17:46:51 +0800

    UserInfo对象

--- src/main/java/com/mybug/service/impl/UserServiceImpl.java
+++ src/main/java/com/mybug/service/impl/UserServiceImpl.java
@@ -1,14 +1,23 @@
 package com.mybug.service.impl;
 
 import com.mybug.po.UserInfoPo;
+import com.mybug.repository.UserRepository;
 import com.mybug.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
- * Created by Administrator on 2017/12/22.
+ * Created by TienChyi on 2017/12/22.
  */
+@Service
 public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserRepository userRepository;
     @Override
-    public UserInfoPo save() {
+    public UserInfoPo userSave() {
+        //UserInfoPo userInfoPo = new UserInfoPo();
+        //userInfoPo.setId(1);
+        userRepository.findById(1);
         return null;
     }
 }
