3c7df2ef33c1da9d843dd974d7b44342273318c1
Tienchyi <tq96321@163.com>
2017-12-24 20:14:02 +0800

    添加用户登录服务

--- src/main/java/com/mybug/service/UserService.java
+++ src/main/java/com/mybug/service/UserService.java
@@ -1,10 +1,14 @@
 package com.mybug.service;
 
+import com.mybug.bo.result.ResultBo;
 import com.mybug.po.UserInfoPo;
 
 /**
  * Created by TienChyi on 2017/12/22.
  */
 public interface UserService {
-    UserInfoPo userSave();
+    //测试
+    //UserInfoPo userSave(UserInfoPo userInfoPo);
+    //登录
+    ResultBo userLogin(UserInfoPo userInfoPo);
 }
--- src/main/java/com/mybug/service/impl/UserServiceImpl.java
+++ src/main/java/com/mybug/service/impl/UserServiceImpl.java
@@ -1,23 +1,44 @@
 package com.mybug.service.impl;
 
+import com.mybug.bo.result.ResultBo;
 import com.mybug.po.UserInfoPo;
-import com.mybug.repository.UserRepository;
+import com.mybug.repository.UserInfoRepository;
 import com.mybug.service.UserService;
+import com.mybug.util.ResultUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 /**
  * Created by TienChyi on 2017/12/22.
  */
+
+
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
-    private UserRepository userRepository;
+    private UserInfoRepository userInfoRepository;
+
     @Override
-    public UserInfoPo userSave() {
-        //UserInfoPo userInfoPo = new UserInfoPo();
-        //userInfoPo.setId(1);
-        userRepository.findById(1);
-        return null;
+    public ResultBo userLogin(UserInfoPo userInfoPo) {
+        UserInfoPo user = userInfoRepository.findByUserName(userInfoPo.getUserName());
+        logger.info("查到的用户信息：" + user.toString());
+        if (user == null) {
+            return ResultUtil.faild("1001", "用户不存在");
+        }
+        if (userInfoPo.getUserPwd().equals(user.getUserPwd())) {
+            HashMap<Object, Object> userMap = new HashMap<>();
+            userMap.put("userType", user.getUserType());
+            return ResultUtil.success(userMap);
+        } else {
+            logger.info("密码不匹配");
+            return ResultUtil.faild("1002", "密码错误");
+        }
     }
 }
