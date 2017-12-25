bd5cd66617a08e6afbb9d5b29a79568865ac44d0
Tienchyi <tqq96321@163.com>
2017-12-25 15:08:00 +0800

    添加返回项目列表

--- src/main/java/com/mybug/service/impl/UserServiceImpl.java
+++ src/main/java/com/mybug/service/impl/UserServiceImpl.java
@@ -3,7 +3,9 @@ package com.mybug.service.impl;
 import com.mybug.bo.ReqBo;
 import com.mybug.bo.result.ResultBo;
 import com.mybug.po.UserInfoPo;
+import com.mybug.po.UserProjectPo;
 import com.mybug.repository.UserInfoRepository;
+import com.mybug.repository.UserProjectRepository;
 import com.mybug.service.UserService;
 import com.mybug.util.ResultUtil;
 import org.slf4j.Logger;
@@ -12,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by TienChyi on 2017/12/22.
@@ -25,18 +28,30 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserInfoRepository userInfoRepository;
+    @Autowired
+    private UserProjectRepository userProjectRepository;
 
     @Override
     public ResultBo userLogin(ReqBo reqBo) {
         UserInfoPo user = userInfoRepository.findByUserName(reqBo.getUserName());
         logger.info("查到的用户信息：" + user.toString());
+        //判断用户存不存在
         if (user == null) {
             return ResultUtil.faild("1001", "用户不存在");
         }
+        //判断密码是否匹配
         if (reqBo.getUserPwd().equals(user.getUserPwd())) {
+            //返回Map
             HashMap<Object, Object> userMap = new HashMap<>();
+            HashMap<Object, Object> projectMap = new HashMap<>();
+            //查找用户所属项目List
+            List<UserProjectPo> projectList = userProjectRepository.findByUserId(user.getId());
+            for (UserProjectPo userProject : projectList){
+                projectMap.put(userProject.getProjectId(),userProject.getProjectName());
+            }
             userMap.put("userType", user.getUserType());
             userMap.put("userId",user.getId());
+            userMap.put("userProject",projectMap);
             return ResultUtil.success(userMap);
         } else {
             logger.info("密码不匹配");
