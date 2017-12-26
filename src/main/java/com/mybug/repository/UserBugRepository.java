85bfa049d668786ae5689ff9229c3ffd85c71ca2
Tienchyi <tqq96321@163.com>
2017-12-26 11:07:13 +0800

    添加查询项目下用户bug接口

--- src/main/java/com/mybug/repository/UserBugRepository.java
+++ src/main/java/com/mybug/repository/UserBugRepository.java
@@ -3,8 +3,11 @@ package com.mybug.repository;
 import com.mybug.po.UserBugPo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/12/25 0025.
  */
 public interface UserBugRepository extends JpaRepository<UserBugPo, Integer> {
+    List<UserBugPo> findByUserIdAndProjectId(Integer userId,Integer projectId);
 }
