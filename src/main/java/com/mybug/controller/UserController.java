7090354f32f07101504672a3a826ce54928413fa
Tienchyi <tqq96321@163.com>
2017-12-26 11:07:57 +0800

    添加查询项目下用户bug和项目下所有用户功能

--- src/main/java/com/mybug/controller/UserController.java
+++ src/main/java/com/mybug/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.mybug.controller;
 
 import com.mybug.bo.ReqBo;
+import com.mybug.bo.ReqUserBugBo;
 import com.mybug.bo.result.ResultBo;
-import com.mybug.service.impl.UserServiceImpl;
+import com.mybug.service.UserService;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by TienChyi on 2017/12/22.
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
-    private UserServiceImpl userService;
+    private UserService userService;
 
     /**
      * 登录
@@ -28,5 +27,17 @@ public class UserController {
     @ResponseBody
     public ResultBo userSave(ReqBo reqBo, HttpServletRequest request){
         return userService.userLogin(reqBo);
+    }
+
+    /**
+     * 查询项目下的用户的bug
+     * @param reqUserBugBo
+     * @param request
+     * @return
+     */
+    @GetMapping("userbuginproject")
+    @ResponseBody
+    public ResultBo userBug(ReqUserBugBo reqUserBugBo,HttpServletRequest request){
+        return userService.userBug(reqUserBugBo);
     }
 }
